06a22ff9092c704b141bcdf4e45b256f5da4d413
Rabdeep Singh <rehbar797@Mac.lan>
2026-04-27 01:27:29 -0400

    reverted config for vercel + fixes

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/mixmatch-website',
-  assetPrefix: '/mixmatch-website',
-  trailingSlash: true,
-}
-
-module.exports = nextConfig
