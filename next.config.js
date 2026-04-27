bb2d3be87b5166ff011a28aa4a65d9379fb58f0e
Rabdeep Singh <rehbar797@Mac.lan>
2026-04-27 01:03:36 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/mixmatch-website',
+  assetPrefix: '/mixmatch-website',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
